c031a4398a68637b6b197216c9eb1c94ad37003c
chetan garg <67314462+chetangarg1905@users.noreply.github.com>
2023-05-11 00:32:58 +0530

    Update streamlit_app.py
    pull data into panda and show on screen

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,6 @@ streamlit.text('🐔 Hard-boiled free-range egg')
 streamlit.text('🥑🍞Avocado  Toast')
 streamlit.header('🍌🥭Build your own fruit smoothie🥝🍇')
 
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
