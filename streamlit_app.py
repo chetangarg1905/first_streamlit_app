7599e4e9fb04ddc61f29947f0a1d8460695653f5
chetan garg <67314462+chetangarg1905@users.noreply.github.com>
2023-05-11 00:35:03 +0530

    Update streamlit_app.py
    user interaction in panda df

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,9 @@ streamlit.header('🍌🥭Build your own fruit smoothie🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Let's put a pick list here so they can pick the fruit they want to include
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
