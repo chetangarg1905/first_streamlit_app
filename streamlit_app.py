8182a8081e350b20776e715505d97612591dd34c
chetan garg <67314462+chetangarg1905@users.noreply.github.com>
2023-05-13 00:41:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urlib.error import URLError
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
   
